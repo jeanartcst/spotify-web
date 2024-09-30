870bd7ee28bd8f880a2bb9c14b876ded1682e716
Jean <jeanartcst@gmail.com>
2024-09-30 20:55:40 -0300

    Window logic changed, removed some absolute divs.

--- src/components/main-container/index.tsx
+++ src/components/main-container/index.tsx
@@ -1,7 +1,9 @@
 export function MainContainer() {
   return (
-    <div className="flex flex-col w-full bg-pink-500">
-      <span>teste</span>
+    // Tamanho de referência da div 1348 x 717 {todas as divs centralizadas tem 717 de altura}
+
+    <div className="left-[110px] top-24 mt-2 rounded-sm flex-col w-3/4 h-5/6 bg-[#121212]">
+      <span>{"tamanho de referência: 1348 "}</span>
     </div>
   );
 }
--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -1,14 +1,21 @@
 import { Footer } from "./footer";
+import { MainContainer } from "./main-container";
 import { NavBar } from "./nav-bar";
 import { SideBar } from "./side-bar";
 // import { MainContainer } from "./main-container";
 
 export function Screen() {
   return (
-    <div className="h-screen m-0 bg-black flex flex-row ">
+    <div className="h-screen flex flex-col ">
       <NavBar />
-      <SideBar />
-      {/* <MainContainer /> */}
+      <div className="flex flex-row items-center justify-between h-[717px]">
+        <SideBar />
+        <MainContainer />
+        <div className="flex-auto mx-4 bg-blue-600 h-full">
+          {" "}
+          <span>teste</span>
+        </div>
+      </div>
       <Footer />
     </div>
   );
